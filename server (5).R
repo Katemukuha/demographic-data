559402ab9bdb475c61fbfda74d5dad5a275b7b56
Katemukuha <156945330+Katemukuha@users.noreply.github.com>
2024-04-15 23:04:52 +0300

    Add files via upload

---
+++ server (5).R
@@ -0,0 +1,20 @@
+# Load libraries
+library(shiny)
+library(tidyverse)
+
+# Read in data
+adult <- read_csv("adult.csv")
+# Convert column names to lowercase for convenience 
+names(adult) <- tolower(names(adult))
+
+# Define server logic
+shinyServer(function(input, output) { df_country <- reactive({ adult %>% filter(native_country == input$country)})
+  # TASK 5: Create logic to plot histogram or boxplot
+output$p1 <- renderPlot({
+  if (input$graph_type == "histogram") {ggplot(df_country(), aes_string(x = input$continuous_variable)) +
+geom_histogram(bins = 30) + labs(y = "Number of people", title = paste("Trend of ", input$continuous_variable)) + facet_wrap(~prediction)
+}
+  else {ggplot(df_country(), aes_string(y = input$continuous_variable)) + geom_boxplot() + coord_flip() + labs(y = "Number of people", title = paste("Trend of ", input$continuous_variable)) + facet_wrap(~prediction) 
+  }})
+})
+
